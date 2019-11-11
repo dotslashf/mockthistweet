3d30549077d195b7fe65b585ca37647cf9aa6e91
fadhluu <fadhloe@gmail.com>
2019-11-12 00:17:48 +0700

    transforming text

---
+++ kalimat.py
@@ -0,0 +1,19 @@
+class Kalimat:
+    def __init__(self, kalimat):
+        self.kalimat = kalimat
+
+    def transform(self):
+        kataJadi = ''
+        kalimat = [i for j in self.kalimat.split() for i in (j, ' ')][:-1]
+        for kata in kalimat:
+            for i, huruf in enumerate(kata):
+                if huruf.isupper():
+                    huruf = huruf.lower()
+                if i % 2 != 0:
+                    huruf = huruf.upper()
+                kataJadi += huruf
+        print(kataJadi)
+
+
+k = Kalimat('Ya kalau lu belajar budaya yg serius lah, bercandain tarian mana itu bagian dari budaya suatu kelompok masyarakat, lu bercandain kaya gitu. belajar ada pakem-pakemnya. masih juga lu belain? hadeh')
+k.transform()
