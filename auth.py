2b3b724980553e834e48a91f1cf8bcbda8405ef2
fadhluu <fadhlurahman.i@gmail.com>
2019-11-18 15:46:09 +0700

    init

--- auth.py
+++ auth.py
@@ -15,20 +15,3 @@ def authentication():
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
     return auth
-
-
-# text = ''
-# for i in range(10):
-#     text += '-'
-#     if i == 9:
-#         print(text)
-
-# auth = authentication()
-# api = tweepy.API(auth)
-# tweet = api.get_status(1195779901380390913)
-# mention_tl = api.mentions_timeline()
-# for mention in mention_tl:
-#     print(mention.text)
-
-# for tweet in tweepy.Cursor(api.mentions_timeline, since_id=1194930186497581057, tweet_mode="extended").items():
-#     print(tweet.full_text)
