a0e77057aec407ffd00c70a4d6f70e92efc92634
fadhluu <fadhlurahman.i@gmail.com>
2019-11-18 17:51:42 +0700

    add method main

--- app.py
+++ app.py
@@ -1,33 +1,43 @@
 import sys
-import tweepy
 import time
 import os
-import random
-from kalimat import Kalimat
-from generator import drawText
-from auth import authentication
+from dotenv import load_dotenv
+from bot import Twitter
 from load import loadData, writeData
 
-:)))))))))))))))))))))) GANTI FILE
+load_dotenv()
+
+CONSUMER_KEY = os.getenv("CONSUMER_KEY")
+CONSUMER_SECRET = os.getenv("CONSUMER_SECRET")
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
+FILE_LAST_ID = os.getenv("FILE_LAST_ID")
 
 
-while True:
-    last_id = loadData(FILE_LAST_ID)
-    last_id = int(last_id[-1])
-    since_id = getMentionTweet(triggeringWords, last_id, errorCode)
+def main(ck, cs, at, ats):
+    bot = Twitter(ck, cs, at, ats)
 
-    print("------------------------------------------------",
-          "\n| newest tweet: ", since_id,
-          "\n| oldest tweet: ", last_id,
-          "\n------------------------------------------------\n")
+    while True:
+        last_id = loadData(FILE_LAST_ID)
+        last_id = int(last_id[-1])
+        since_id = bot.get_mention_tweet(last_id)
 
-    if (last_id != since_id):
-        writeData(FILE_LAST_ID, str(since_id))
-    else:
-        print('no new mention')
+        print("------------------------------------------------",
+              "\n| newest tweet: ", since_id,
+              "\n| oldest tweet: ", last_id,
+              "\n------------------------------------------------\n")
 
-    for sec in range(180, 0, -1):
-        sys.stdout.write("\r")
-        sys.stdout.write("{:2d} second to check mention.\r".format(sec))
-        sys.stdout.flush()
-        time.sleep(1)
+        if (last_id != since_id):
+            writeData(FILE_LAST_ID, str(since_id))
+        else:
+            print('no new mention')
+
+        for sec in range(180, 0, -1):
+            sys.stdout.write("\r")
+            sys.stdout.write("{:2d} second to check mention.\r".format(sec))
+            sys.stdout.flush()
+            time.sleep(1)
+
+
+if __name__ == "__main__":
+    main(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
