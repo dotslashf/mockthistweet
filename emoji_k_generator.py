c416a4cf45fd4e3d35f9ef429fe4ccbf6fa90f31
fadhluu <fadhlurahman.i@gmail.com>
2019-11-19 20:38:32 +0700

    add b pattern

--- emoji_k_generator.py
+++ emoji_k_generator.py
@@ -18,58 +18,96 @@ class Emoji:
                 char_emoji = emoji.emojize(k[0])
         self.emoji = char_emoji
 
-    def create_pattern(self):
-        b1 = [0, 1, 12, 13]
-        b2 = [0, 1, 7, 8]
-        b3 = [0, 1, 2, 3]
+    def create_pattern(self, pattern):
 
         finalText = ''
-        for i in range(6):
-            for j in range(14):
-                if i == 0 and j in b1:
-                    finalText += self.emoji
-                    if j == 13:
-                        finalText += '\n'
-                elif i == 1 and j in b2:
-                    finalText += self.emoji
-                    if j == 8:
-                        finalText += '\n'
-                elif i == 2 and j in b3:
-                    finalText += self.emoji
-                    if j == 3:
-                        finalText += '\n'
-                elif i == 3 and j in b3:
-                    finalText += self.emoji
-                    if j == 3:
-                        finalText += '\n'
-                elif i == 4 and j in b2:
-                    finalText += self.emoji
-                    if j == 8:
-                        finalText += '\n'
-                elif i == 5 and j in b1:
-                    finalText += self.emoji
-                    if j == 13:
-                        finalText += self.sentence[1:(len(self.sentence))]
+        if pattern == 'k':
+            b1 = [0, 1, 12, 13]
+            b2 = [0, 1, 7, 8]
+            b3 = [0, 1, 2, 3]
 
-                else:
-                    if i == 0 and j in range(2, 11):
-                        finalText += ' '
-                    elif i == 1 and j in range(2, 9):
-                        finalText += ' '
-                    elif i == 4 and j in range(2, 9):
-                        finalText += ' '
-                    elif i == 5 and j in range(2, 11):
-                        finalText += ' '
+            for i in range(6):
+                for j in range(14):
+                    if i == 0 and j in b1:
+                        finalText += self.emoji
+                        if j == 13:
+                            finalText += '\n'
+                    elif i == 1 and j in b2:
+                        finalText += self.emoji
+                        if j == 8:
+                            finalText += '\n'
+                    elif i == 2 and j in b3:
+                        finalText += self.emoji
+                        if j == 3:
+                            finalText += '\n'
+                    elif i == 3 and j in b3:
+                        finalText += self.emoji
+                        if j == 3:
+                            finalText += '\n'
+                    elif i == 4 and j in b2:
+                        finalText += self.emoji
+                        if j == 8:
+                            finalText += '\n'
+                    elif i == 5 and j in b1:
+                        finalText += self.emoji
+                        if j == 13:
+                            finalText += self.sentence[1:(len(self.sentence))]
+
+                    else:
+                        if i == 0 and j in range(2, 11):
+                            finalText += ' '
+                        elif i == 1 and j in range(2, 9):
+                            finalText += ' '
+                        elif i == 4 and j in range(2, 9):
+                            finalText += ' '
+                        elif i == 5 and j in range(2, 11):
+                            finalText += ' '
+
+        elif pattern == 'b':
+            b1 = [0, 1, 2]
+            b2 = [0, 11]
+
+            for i in range(6):
+                for j in range(12):
+                    if i == 0 and j in b1:
+                        finalText += self.emoji
+                        if j == 2:
+                            finalText += '\n'
+                    elif i == 1 and j in b2:
+                        finalText += self.emoji
+                        if j == 11:
+                            finalText += '\n'
+                    elif i == 2 and j in b1:
+                        finalText += self.emoji
+                        if j == 2:
+                            finalText += '\n'
+                    elif i == 3 and j in b2:
+                        finalText += self.emoji
+                        if j == 11:
+                            finalText += '\n'
+                    elif i == 4 and j in b2:
+                        finalText += self.emoji
+                        if j == 11:
+                            finalText += '\n'
+                    elif i == 5 and j in b1:
+                        finalText += self.emoji
+                        if j == 2:
+                            finalText += self.sentence[1:len(self.sentence)]
+                    else:
+                        if i == 1 and j in range(1, 10):
+                            finalText += ' '
+                        elif i == 3 and j in range(1, 10):
+                            finalText += ' '
+                        elif i == 4 and j in range(1, 10):
+                            finalText += ' '
         return finalText
 
 
-# e = Emoji("kontol")
-# # # b = Twitter(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+# e = Emoji("bacot banget loe")
 
 # re = e.random()
 # print(re)
 # e.pick_emoji(re)
-# jadi = e.create_pattern()
+# jadi = e.create_pattern('b')
 # jadi += "test"
 # print(jadi)
-# # b.api.update_status(status=jadi)
