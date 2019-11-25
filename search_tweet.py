50966e54733d289e9a5a30e242373ffe27fbe0d0
fadhluu <fadhlurahman.i@gmail.com>
2019-11-26 01:08:18 +0700

    init

--- search_tweet.py
+++ search_tweet.py
@@ -1,31 +1,25 @@
+import tweepy
 from bot import Twitter
-from load import loadData
-import os
-from dotenv import load_dotenv
+from db_mongo import Database
 
-load_dotenv()
+db = Database()
+db.connect_db('twitter')
+db.select_col('environment')
 
-CONSUMER_KEY = os.getenv("CONSUMER_KEY")
-CONSUMER_SECRET = os.getenv("CONSUMER_SECRET")
-ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
-ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
+consumer_key = db.find_object('CONSUMER_KEY')
+consumer_secret = db.find_object('CONSUMER_SECRET')
+access_token = db.find_object('ACCESS_TOKEN')
+access_token_secret = db.find_object('ACCESS_TOKEN_SECRET')
 
-bot = Twitter(CONSUMER_KEY, CONSUMER_SECRET,
-              ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+bot = Twitter(consumer_key, consumer_secret, access_token, access_token_secret)
 
-tweet_bug = bot.api.get_status(id=1196782210642333696, include_entities=False)
-tweet = bot.api.get_status(id=1196799891961307136)
+list_tweet = []
 
-# for key, entities in tweet_bug.entities.items():
-#     for value in entities:
-#         print(value['screen_name'])
+for tweet in tweepy.Cursor(bot.api.mentions_timeline,
+                           since_id=1199008704076505092,
+                           max_id=1199009095925161992,
+                           tweet_mode="extended").items():
+    list_tweet.append(tweet.full_text)
 
-# print('\n')
-
-# for tweet in tweet.entities.items():
-#     for a in tweet[1]:
-#         last = list(a.items())[0][-1]
-# print(last)
-
-
-print(tweet_bug.text)
+for t in reversed(list_tweet):
+    print(t)
