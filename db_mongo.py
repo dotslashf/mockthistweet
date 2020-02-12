5687e34b86d402812b94a645dfb371f037dc8f93
fadhlu <fadhlurahman.i@gmail.com>
2020-02-12 10:50:12 +0700

    init

--- db_mongo.py
+++ db_mongo.py
@@ -4,12 +4,8 @@ import os
 
 class Database:
     def __init__(self):
-        db_user = os.environ.get("DB_USER")
-        db_password = os.environ.get("DB_PASS")
-        db_cluster = os.environ.get("DB_CLUSTER")
-
-        self.client = pymongo.MongoClient("mongodb+srv://{}:{}@{}-kdbqm.mongodb.net/test?retryWrites=true&w=majority".format(
-            db_user, db_password, db_cluster))
+        self.client = pymongo.MongoClient(
+            "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false")
         self.db = None
         self.collection = None
 
