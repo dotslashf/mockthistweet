b9afb5c087dd1df80c4a31e7c014ec12d4588608
fadhlu <fadhlurahman.i@gmail.com>
2020-02-13 10:47:35 +0700

    add dictionary files

---
+++ dict.py
@@ -0,0 +1,27 @@
+error_code = {
+    "private_account": [179, "Kalau mau ngemock pikir-pikir juga dong, masa private akun, mana keliatan tweetnya "],
+    "blocked_account": [136, "Botnya dah diblock sama doi, ah ga seru "],
+    "duplicate_tweet": [187, "Duplicated tweet"],
+    "tweet_target_deleted": [144, "Tweetnya udah dihapus, kasian deh lo"],
+    "tweet_target_to_long": [186, "Tweetnya kepanjangan kalau di tambahin emoji, coba format yang lain"],
+    "tweet_deleted_or_not_visible": [385, "Tweet deleted or not visible"],
+    "twitter_over_capacity": [130, "Twitternya lagi overcapacity, next time yah"],
+    "page_does_not_exist": [34, "Pages does not exist"],
+    "suspended_account": [63, "Yang mau dimock dah disuspend, twitter, mampus."]
+}
+
+tweet_text = {
+    "dont_mock": ["Enak aja developernya mau di mock, jangan ngelawak deh ",
+                  " adalah orang yang paling gabut, gak usah nyoba buat ngebuat botnya ngemock diri sendiri."],
+    "follow_dulu": "Follow dulu, kalau gak mau ya mock manual aja yah ",
+    "untag_dong": "Kalau jelasin cara kerja botnya tolong di untag yah, "
+}
+
+file_meme = {"output": ["img/meme_spongebob_output.png", "img/meme_khaleesi_output.png"],
+             "input": ["img/meme_new.png", "img/meme_khaleesi.png"]}
+
+trigger_words = ["please", "pliisi",
+                 "please😂", "please👏",
+                 "please🤮", "please🤢",
+                 "pleasek", "pleaseb", "pleasej",
+                 "please💩", "pleasealay"]
