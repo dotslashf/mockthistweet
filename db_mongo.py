f38d9b8f8c8d93e8a2c07ef909dad98df19302dd
fadhlu <fadhlurahman.i@gmail.com>
2020-02-15 10:31:01 +0700

    add connection local and production

--- db_mongo.py
+++ db_mongo.py
@@ -4,8 +4,17 @@ import os
 
 class Database:
     def __init__(self):
-        self.client = pymongo.MongoClient(
-            "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false")
+
+        if os.environ.get("APP_ENV") == 'LOCAL':
+            self.client = pymongo.MongoClient(
+                "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false")
+        else:
+            db_user = os.environ.get("DB_USER")
+            db_password = os.environ.get("DB_PASS")
+            db_cluster = os.environ.get("DB_CLUSTER")
+            self.client = pymongo.MongoClient(
+                "mongodb+srv://{}:{}@{}-kdbqm.mongodb.net/test?retryWrites=true&w=majority".format(db_user, db_password, db_cluster))
+
         self.db = None
         self.collection = None
 
